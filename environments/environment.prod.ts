59743e9fa2aaba42f06601d8560f3205cd3bffb2
Mulinti Chaitya <2025aa05540@wilp.bits-pilani.ac.in>
2026-03-11 07:39:39 +0530

    commit

---
+++ environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://20.244.84.188:8081'
+};
---
+++ environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://20.244.84.188:8081'
+};
