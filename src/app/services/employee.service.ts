b36767c6864c54ec51f2889b68a6b68ddef2eb83
Mulinti Chaitya <2025aa05540@wilp.bits-pilani.ac.in>
2026-03-12 05:35:02 +0530

    Update employee.service.ts

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,13 +3,15 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Employee } from '../models/employee.model';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private apiUrl = 'http://localhost:8080/api/employees';
+  private apiUrl = `${environment.apiUrl}/api/employees`;
 
   constructor(private http: HttpClient) {}
 
